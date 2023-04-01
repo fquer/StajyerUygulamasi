dba854820bc9a7d0bfc066842a69a71fc0bbe20b
Kaan Bilgi <54486031+fquer@users.noreply.github.com>
2023-04-01 04:19:34 +0300

    register page added
    register page check mail, empty fields, update db

---
+++ Pages/Register.cshtml.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using StajyerUygulamasi.Model;
+
+namespace StajyerUygulamasi.Pages
+{
+    public class RegisterModel : PageModel
+    {
+        private readonly ILogger<RegisterModel> _logger;
+        private readonly ApplicationDbContext _db;
+
+        public RegisterModel(ILogger<RegisterModel> logger, ApplicationDbContext db)
+        {
+            _logger = logger;
+            _db = db;
+        }
+
+        [BindProperty]
+        public Stajyer Stajyer { get; set; }
+
+        public void OnGet()
+        {
+
+        
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid) {
+
+                var isMailValid = await _db.Stajyer.FirstOrDefaultAsync(dbStajyer => dbStajyer.Email == Stajyer.Email);
+
+                if (isMailValid == null) {
+                    await _db.Stajyer.AddAsync(Stajyer);
+                    await _db.SaveChangesAsync();
+                    return RedirectToPage("Login");
+                }
+                TempData["RegisterMessage"] = "Girilen mail adresi başka bir kullanıcı tarafından kullanılıyor!";
+                return Page();
+            } 
+            else {
+                TempData["RegisterMessage"] = "Lütfen eksik bilgileri doldurunuz.";
+                return Page();
+            }
+        }
+    }
+}
